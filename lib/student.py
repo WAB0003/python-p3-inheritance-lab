36f825b2d1d837dcb2d329259bf41f3c3fd1d825
Bill Brown <bill.a.brown90@gmail.com>
2023-04-09 13:19:14 -0700

    basics of inheritance

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
+# pytest lib/testing/student_test.py 
+import ipdb
 
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = list()
     
-    def learn(self):
-        pass
+    def learn(self,knowledge):
+        if type(knowledge) == str:
+            self.knowledge.append(knowledge)
